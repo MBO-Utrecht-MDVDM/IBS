8caf873bd938fa635ee1d4f3d2f677a733b7a2ff
CYB3RW07F <314994@student.mboutrecht.nl>
2019-05-23 13:49:35 +0200

    Final push omg yes

--- app/Http/Controllers/UsersAnswersController.php
+++ app/Http/Controllers/UsersAnswersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Questionnaire;
 use App\UsersAnswers;
+use Illuminate\Support\Facades\Input;
 
 
 class UsersAnswersController extends Controller
@@ -17,10 +18,15 @@ class UsersAnswersController extends Controller
     public function store($user_id, Request $request)
     {
         \Log::info($request->all());
+        foreach($request -> all() as $item => $value){
+        if($item === "_token"){
+            continue;
+        }
         $newAnswer = new UsersAnswers();
         $newAnswer->user_id = $user_id;
-        $newAnswer->answer_id = $request->all()[1];
+        $newAnswer->answer_id = $value;
         $newAnswer->save();
+        }
         // UsersAnswers::create($request->all() + ['user_id' => $user_id]);
         return redirect('/');
     }
